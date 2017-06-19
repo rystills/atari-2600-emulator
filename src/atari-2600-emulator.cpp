7e65676e888d6aa3e5fe658e8b0398d8e050b2d0
Ryan Stillings <rystills@gmail.com>
2017-06-19 00:56:27 -0400

    document existing code before moving forward

--- src/atari-2600-emulator.cpp
+++ src/atari-2600-emulator.cpp
@@ -3,22 +3,37 @@
 #include "tinydir/tinydir.h"
 #include <stdio.h>
 
-int main() {
+/* @param fileDir - the directory in which to list files
+   @effects list the files in the input directory
+*/
+void listFiles(char* fileDir) {
+	//init tinydir directory to fileDir
 	tinydir_dir dir;
-	tinydir_open(&dir, "./roms");
-	
+	tinydir_open(&dir, fileDir);
+
+	//iterate over all files in dir
 	while (dir.has_next) {
-	    tinydir_file file;
-	    tinydir_readfile(&dir, &file);
+		tinydir_file file;
+		tinydir_readfile(&dir, &file);
 
-	    printf("%s", file.name);
-	    if (file.is_dir)
-	    {
-	        printf("/");
-	    }
-	    printf("\n");
+		//print file name, followed by a '/' if the file is a directory
+		printf("%s", file.name);
+		if (file.is_dir)
+		{
+			printf("/");
+		}
+		printf("\n");
 
-	    tinydir_next(&dir);
+		//proceed to the next file
+		tinydir_next(&dir);
 	}
+
+	//close dir once we're finished reading files
 	tinydir_close(&dir);
+}
+
+int main() {
+	//hard-code romName for testing
+	std::string romName = "ADVNTURE.BIN";
+	listFiles("./roms");
 }
